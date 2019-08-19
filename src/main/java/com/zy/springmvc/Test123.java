de3a67fd4064fa2c85ea45e0d38294861eda067e
ZhangYDevelop <2389311433@qq.com>
2019-08-19 21:17:45 +0800

    修改  bug

---
+++ src/main/java/com/zy/springmvc/Test123.java
@@ -0,0 +1,12 @@
+package com.zy.springmvc;
+
+/**
+ * @AUTHOR zhangy
+ * 2019-08-19  21:16
+ */
+public class Test123 {
+
+    public void getName(String id) {
+        System.out.println("nihao");
+    }
+}
